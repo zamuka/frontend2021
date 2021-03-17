81d6e996c46ba08bb3e7b5e4444f7ee1e246c395
GulenkoAnastasia <nansi0307@yahoo.com>
2021-03-17 15:12:38 +0300

    homework s03e04-counting-letters is done

--- homework/s03e04-counting-letters.js
+++ homework/s03e04-counting-letters.js
@@ -4,7 +4,7 @@ const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
 
 // Обратиться к каждому символу (букве) можно используя квадратные скобки
 // Например, это 'L', так как многое в цифровом мире начинается с ноля.
-const firstCharacter = message[0];
+// const firstCharacter = message[0];
 // переменная firstCharacter вам не нужна и тут только для примера
 
 // Длину строки можно узнать из свойства length
@@ -12,11 +12,26 @@ const messageLength = message.length;
 
 
 // начнем считать с ноля
-const aLetterCount = 0;
+let aLetterCount = 0; //значение у aLetterCount будет менятся, поэтому поменяла на let
 
 
 // А тут можно написать цикл, который переберет все буквы по очереди и если это 'i' - увеличит счетчик.
 
+let i = 0;
+while (i <= messageLength){
+    i = i + 1;
+    if (message[i] === 'i') {
+    aLetterCount = aLetterCount + 1;
+    }    
+}
+
+//Можно и так:
+let aLetterCount = 0;
+for (let letter of message){
+    if (letter === 'i'){
+        aLetterCount = aLetterCount + 1;
+    }
+}
 
 // вывод результата
 console.log(`Нашлось ${aLetterCount} букв 'i'.`);
