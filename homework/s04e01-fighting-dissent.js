0f7b108de153d58274293531d625a7e2940520c3
Yevhen <zhekahome@gmail.com>
2021-03-21 19:58:09 +0200

    Home work Fighting dissent is done

---
+++ homework/s04e01-fighting-dissent.js
@@ -0,0 +1,24 @@
+const sourceMessage = 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let sousceText = sourceMessage.split('');
+let resultMessage = [];
+let vowelCounter = 0;
+
+for (let char of sousceText) {
+    let areCharVowel = false;
+    
+    for (let key of vowels) {
+        if (key === char) {
+            vowelCounter = vowelCounter + 1;
+            areCharVowel = true;
+            break;
+        }
+    }
+
+    if (!areCharVowel) {
+        resultMessage.push(char);
+    }
+}
+
+console.log('There are ' + vowelCounter + ' vowels in this text.');
+console.log(resultMessage.join(''));
