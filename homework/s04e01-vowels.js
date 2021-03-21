9fa3130454ed429defeeefd8497b66f08a877bf3
Yulia Khitrukhina <yulyashenkaa@gmail.com>
2021-03-21 08:29:59 -0700

    one more solution for Vowels

--- homework/s04e01-vowels.js
+++ homework/s04e01-vowels.js
@@ -15,3 +15,21 @@ for (let char of inputText) {
     }
 }
 inputString = result.join('');
+
+//one more solution
+
+let inputString = 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...';
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+let isVowel;
+let result = '';
+for(let char of inputString) {
+    isVowel = false;
+    for(let i = 0; i < vowels.length; i++) {
+        isVowel = char.toLowerCase() === vowels[i];
+        if (isVowel) break;
+    }
+    if(!isVowel) {
+        result = result + char;
+    }
+}
+inputString = result;
