595d983f05eac1fc31ab4850f54de399faa9cb73
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-03-25 17:52:34 +0200

    hw_9_function

---
+++ homework/hw_9_function.js
@@ -0,0 +1,29 @@
+// Напишите функцию, которая посчитает и вернет количество букв 'i' в строке
+const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+function getILetterCount(str) {
+    let result = 0;
+    for (let i = 0; i < str.length; i = i + 1) {
+        let currentLatter = str[i];
+        if (currentLatter === 'i') {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+const count = getILetterCount(message);
+console.log(count);
+
+
+// Напишите функцию, которая посчитает в строке заданную букву, которую принимает вторым параметром
+const message = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const getLetterCount = function (str, x) {
+    let result = 0;
+    for(let i = 0; i < str.length; i = i + 1) {
+        if (str[i] === x) {
+            result = result + 1;
+        }
+    }
+    return result;
+}
+const aLetterCount = getLetterCount(message, 'a');
+console.log(aLetterCount);
