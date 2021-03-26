ee1d538cb91a95b14cf485082afbfcac946834c6
Yevhen <zhekahome@gmail.com>
2021-03-26 16:33:54 +0200

    Homework s06 is done

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,26 @@
 function buttonClick() {
-  // ваш код тут
+ 
+  const width = document.querySelector('[name="gridWidth"]').value;
+  const height = document.querySelector('[name="gridHeight"]').value;
+  const result = document.querySelector('#result');
+
+  result.innerText = width * height;
+
   alert('Button clicked');
 }
+
+
+
+//Вариант: document.forms.[form name].elements.[name]
+
+/* function buttonClick() {
+  
+  const width = document.forms['cardForm'].elements['gridWidth'].value;
+  const height = document.forms['cardForm'].elements['gridHeight'].value;
+  const result = document.querySelector('#result');
+  
+
+  result.innerText = width * height;
+
+  alert('Button clicked');
+} */
