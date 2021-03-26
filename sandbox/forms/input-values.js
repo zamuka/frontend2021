d102e9e8dacfef76a2e1dc37bc4c854491e4f287
Dmytro Chyrva <dchyrva413@gmail.com>
2021-03-26 13:53:18 +0200

    Homework Done

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,14 @@
 function buttonClick() {
-  // ваш код тут
-  alert('Button clicked');
+	// ваш код тут
+
+	// const result = document.querySelector('#result');
+	const width = document.querySelectorAll('input')[0].value;
+
+	// const width = document.forms['cardForm'].elements['gridWidth'].value;
+	const height = document.querySelectorAll('input')[1].value;
+	// const height = document.forms['cardForm'].elements['gridHeight'].value;
+
+	result.innerText = width * height;
+
+	// alert('Button clicked');
 }
