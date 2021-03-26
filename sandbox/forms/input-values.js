a5a3a6abcfa0f4517ceabe8bcaedd750247a4053
GulenkoAnastasia <nansi0307@yahoo.com>
2021-03-26 14:36:21 +0300

    homework input-values uploaded

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,12 +1,12 @@
 function buttonClick() {
-  let inputWidthValue = document.querySelector('[name="gridWidth"]').value;
-  let inputHeightValue = document.querySelector('[name="gridHeight"]').value;
-  let resultElement = document.querySelector('#result');
+  const inputWidthValue = document.querySelector('[name="gridWidth"]').value;
+  const inputHeightValue = document.querySelector('[name="gridHeight"]').value;
+  const resultElement = document.querySelector('#result');
   resultElement.innerText = inputWidthValue * inputHeightValue;
   alert('Button clicked');
 }
 
-// Наверное можно было все одной строкой прописать, но первый вариант более понятный.
+// // Наверное можно было все одной строкой прописать, но первый вариант более понятный.
 function buttonClick() {
   document.querySelector('#result').innerText = document.querySelector('[name="gridWidth"]').value * document.querySelector('[name="gridHeight"]').value;
   alert('Button clicked');
