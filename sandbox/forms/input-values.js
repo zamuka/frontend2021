1efa1ddfd78aab7df75a3f421e44f4d481695649
GulenkoAnastasia <nansi0307@yahoo.com>
2021-03-26 14:27:20 +0300

    homework input-values is done

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,13 @@
 function buttonClick() {
-  // ваш код тут
+  let inputWidthValue = document.querySelector('[name="gridWidth"]').value;
+  let inputHeightValue = document.querySelector('[name="gridHeight"]').value;
+  let resultElement = document.querySelector('#result');
+  resultElement.innerText = inputWidthValue * inputHeightValue;
+  alert('Button clicked');
+}
+
+// Наверное можно было все одной строкой прописать, но первый вариант более понятный.
+function buttonClick() {
+  document.querySelector('#result').innerText = document.querySelector('[name="gridWidth"]').value * document.querySelector('[name="gridHeight"]').value;
   alert('Button clicked');
 }
