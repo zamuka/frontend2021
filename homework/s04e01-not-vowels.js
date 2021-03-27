a5c8aa04a3ef0b7e8ac1ace85ad4dafb2645d1a7
Vlada-khom <vvlada2610@gmail.com>
2021-03-27 12:48:38 +0200

    add function in file

--- homework/s04e01-not-vowels.js
+++ homework/s04e01-not-vowels.js
@@ -1,6 +1,6 @@
 let inputString = 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...';
-let arrString = inputString.split('');
-let result = [];
+// let arrString = inputString.split('');
+let result1 = [];
 const arrVowels = [
     'a',
     'e',
@@ -17,10 +17,36 @@ for(let char of inputString) {
         }
     }
     if(!theVowels) {
-        result.push(char);
+        result1.push(char);
     }
 }
 
-console.log(result.join(''));
+console.log(result1.join(''));
 
+
+
+// Вывела соласные функцией.
+
+
+function returnOfConsonants(inputString) {
+    let result2 = '';
+    for(let i = 0; i < inputString.length; i++) {
+        const letter = inputString[i].toLocaleLowerCase();
+        if (!isVowel(letter)) {
+            result2 = result2 + letter;
+        }
+    }
+    return result2;
+}
+
+function isVowel(letter) {
+    if (letter === 'a' || letter === 'e' || letter === 'i' || letter === 'o' || letter === 'u') {
+        return true;
+    }
+}
+
+
+
+result2= returnOfConsonants(inputString);
+console.log(result2);
 
