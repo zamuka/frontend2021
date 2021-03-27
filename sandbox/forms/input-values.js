a81253f3c9bc518899efaa6720a5e39b31708d45
Vlada-khom <vvlada2610@gmail.com>
2021-03-27 14:52:06 +0200

    done hw6

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,18 @@
 function buttonClick() {
-  // ваш код тут
+  const result = document.querySelector('#result');
+  const width = document.querySelector('[name = "gridWidth"]').value;
+  const height = document.querySelector('[name = "gridHeight"]').value;
+  result.innerText = width * height;
+  alert('Button clicked');
+}
+
+// 2 вариант
+function buttonClick() {
+  const result = document.querySelector('#result');
+  const width = document.forms['cardForm'].elements['gridWidth'].value;
+  const height = document.forms['cardForm'].elements['gridHeight'].value;
+
+  result.innerText = width * height;
+
   alert('Button clicked');
 }
