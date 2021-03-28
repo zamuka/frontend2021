585cf7c49b0166ab047af8850bcb84de5de2a817
Solamatin Mikhail <solamatinm@smart-ui.pro>
2021-03-28 23:04:14 +0300

    HW_6 input-values done

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,20 @@
 function buttonClick() {
-  // ваш код тут
+  const width = document.forms['cardForm'].elements['gridWidth'];
+  const height = document.forms['cardForm'].elements['gridHeight'];
+  console.log(width);
+  console.log(height);
+
+  const inputWidth = document.querySelector('.form-control[name=gridWidth]');
+  const inputHeight = document.querySelector('.form-control[name=gridHeight]');
+  const resWidget = document.getElementById('result');
+
+  let widthValue = inputWidth.value;
+  let heightValue = inputHeight.value;
+
+  let res = widthValue * heightValue;
+  resWidget.innerHTML = res;
+
   alert('Button clicked');
 }
+
+window.addEventListener('load', buttonClick);
