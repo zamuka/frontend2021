844b39260eeed6ff57d43f8d53781c26aef76709
Yulia Khitrukhina <yulyashenkaa@gmail.com>
2021-03-28 13:04:24 -0700

    added solution for input-values

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,19 @@
+window.addEventListener('DOMContentLoaded', DOMContentLoaded);
+let inputWidth;
+let inputHeight;
+let result;
+let isDocumentLoaded;
+
+function DOMContentLoaded(){
+  inputWidth = document.querySelector('#inputWidth');
+  inputHeight = document.forms.cardForm.elements.gridHeight;
+  result = document.querySelector('#result');
+  isDocumentLoaded = true;
+}
+
 function buttonClick() {
-  // ваш код тут
-  alert('Button clicked');
+  if(isDocumentLoaded) {
+    result.innerHTML = inputWidth.value * inputHeight.value;
+    alert('Button clicked');
+  }
 }
