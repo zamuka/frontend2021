ea4e0605795c746322980e5b9ab58bdd947f0796
Kate Gubchenko <porotunya@gmail.com>
2021-03-28 19:06:53 +0300

    add forms homework

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,9 @@
 function buttonClick() {
-  // ваш код тут
+  let selectWidth = document.forms.cardForm.elements.gridWidth.value;
+  let selectHeight = document.forms.cardForm.elements.gridHeight.value;
+  
+  let result = document.querySelector('#result');
+  result.innerText = selectWidth * selectHeight;
   alert('Button clicked');
 }
+
