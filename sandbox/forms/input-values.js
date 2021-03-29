60afc7ed01f9de350715e0ec6f1b42de2258bd76
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-03-29 18:40:02 +0300

    HW_10_done

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,8 @@
 function buttonClick() {
-  // ваш код тут
+  const width = document.querySelector('[name="gridWidth"]').value; /*const width = document.forms['cardForm'].elements['gridWidth'].value;*/
+  const height = document.querySelector('[name="gridHeight"]').value; /*const height = document.forms['cardForm'].elements['gridHeight'].value;*/
+  const result = document.querySelector('#result'); /*const result = document.querySelector('#result');*/
+
+  result.innerText = width * height;
   alert('Button clicked');
 }
