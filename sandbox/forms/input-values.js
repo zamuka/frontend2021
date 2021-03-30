bbd6979f10033b2ee8a95d03dd7c0dc4071a2fd5
sveta.zaletskaya <lana.zaletskaya@gmail.com>
2021-03-30 19:20:43 +0300

    hw_form

--- sandbox/forms/input-values.js
+++ sandbox/forms/input-values.js
@@ -1,4 +1,33 @@
-function buttonClick() {
-  // ваш код тут
-  alert('Button clicked');
+let calculate = 1;
+
+function buttonClick () {
+
+  let inputs = document.querySelectorAll('input');
+
+  for (let i =0; i<inputs.length; i++) {
+
+    let inputValue = inputs[i].value;
+
+    calculate = calculate*inputValue;
+
+  }
+
+  document.getElementById('result').innerText = calculate;
+
+  calculate = 1;
+
+
 }
+
+//or
+
+// function buttonClick () {
+
+//   let inputGridWidth = document.forms.cardForm.elements.gridWidth.value;
+
+//   let inputGridHeight = document.forms.cardForm.elements.gridHeight.value;
+
+//   let calculate = inputGridWidth*inputGridHeight;
+
+//   document.getElementById('result').innerText = calculate;
+// }
