b72c844a09ccf67a8e19ff3fc45e19b112b4dcff
GulenkoAnastasia <nansi0307@yahoo.com>
2021-03-31 10:26:03 +0300

    homework s07e02-odds is done

--- homework/s07e02-odds.js
+++ homework/s07e02-odds.js
@@ -6,6 +6,15 @@
  * @param {number[]} inputNumbers
  * @returns {number[]}
  */
+const sampleArray = [3, 61, 453, 5, 4, 32, 4, 59, 8, 7, 43];
+
 function getOdds(inputNumbers) {
-  // ВАШ КОД ТУТ
+  const oddsArray = [];
+  for (let i = 0; i < inputNumbers.length; i = i + 1) {
+    if (inputNumbers[i] % 2) {
+      oddsArray.push(inputNumbers[i]);
+    }
+  }
+  return oddsArray;
 }
+console.log(getOdds(sampleArray));
