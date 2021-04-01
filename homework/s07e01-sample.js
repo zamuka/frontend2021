b0fc42cfe672a47fccbce091cd649f46b392e920
Vlada-khom <vvlada2610@gmail.com>
2021-04-01 14:10:55 +0300

    hw les 07 done

--- homework/s07e01-sample.js
+++ homework/s07e01-sample.js
@@ -1,13 +1,26 @@
 // 1. напишите функцию, которая вернет случайный элемент массива.
 // пример использования:
 
-// const randomColor = sample(['red', 'green', 'blue', 'orange']);
+const randomColor = ['red', 'green', 'blue', 'orange'] ;
 
+function randomElement(randomColor) {
+    return randomColor[Math.floor(Math.random()*randomColor.length)];
+}
+console.log(randomElement(randomColor));
 // 2. напишите функцию, которая принимает строку и число n, а возвращает
 // новую строку, состоящую из n случайных букв начальной строки.
 // Например
 
-// const result = getRandomLetters('abcdefgh', 4);
+const result = getRandomLetters('abcdefgh', 4);
+
+function getRandomLetters(str, n) {
+    let randomStr = [];
+    for(i = 0; i < n; i = i + 1){
+        randomStr.push(str.charAt(Math.floor(Math.random() * str.length)));
+    }
+    return randomStr.join('');
+}
+console.log(result);
 
 // вернет 4 случайно выбранные буквы в виде строки, возможно, 'gbbc';
 // Буквы могут повторяться.
@@ -15,12 +28,20 @@
 // 3. Пишем функцию, которая принимает объект, а возвращает новый
 // объект, в котором только одно случайное свойство из оригинального
 // например для объекта:
-// const person = {
-//   name: 'Vasya',
-//   age: 20,
-//   occupation: 'student',
-//   height: 170,
-// };
+const person = {
+name: 'Vasya',
+age: 20,
+occupation: 'student',
+height: 170,
+};
 
-// sampleProperty(person) вернет, например,
+function sampleProperty(person) {
+  let objEntries = Object.entries(person);
+  return objEntries[Math.floor(Math.random() * objEntries.length)];
+}
+
+let randomProp = sampleProperty(person);
+console.log (randomProp.join(':'));
+
+//  вернет, например,
 // { age: 20 } или { height: 170 }
--- homework/s07e02-odds.js
+++ homework/s07e02-odds.js
@@ -3,9 +3,16 @@
  * Функция принимает массив чисел, а возвращает новый массив,
  * в котором только нечетные числа из оригинального массива
  * оператор % тут отлично подойдет
- * @param {number[]} inputNumbers
- * @returns {number[]}
- */
-function getOdds(inputNumbers) {
-  // ВАШ КОД ТУТ
+//  * @param {number[]} inputNumbers
+//  * @returns {number[]}
+//  */
+let arr = [1,2,3,4,5,6,7,8,9,10,11,12];
+
+function getOdds(_inputNumbers) {
+
+  let odds = arr.filter(inputNumbers => inputNumbers%2);
+  
+  return odds;
 }
+const arrOdds = getOdds(arr);
+console.log(arrOdds);
