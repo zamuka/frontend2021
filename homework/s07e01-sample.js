5e88799ecf8773f17831b81a6ae6e7122c6ff746
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-04-02 15:51:01 +0300

    hw_sample-done

--- homework/s07e01-sample.js
+++ homework/s07e01-sample.js
@@ -1,26 +1,54 @@
 // 1. напишите функцию, которая вернет случайный элемент массива.
 // пример использования:
 
-// const randomColor = sample(['red', 'green', 'blue', 'orange']);
+const randomColor = sample(['red', 'green', 'blue', 'orange']);
+
+function sample(items) {
+    const randomColor = Math.floor(Math.random()* items.length);
+    
+    return items[randomColor];
+}
 
 // 2. напишите функцию, которая принимает строку и число n, а возвращает
 // новую строку, состоящую из n случайных букв начальной строки.
 // Например
 
-// const result = getRandomLetters('abcdefgh', 4);
+const result = getRandomLetters('abcdefgh', 4);
 
-// вернет 4 случайно выбранные буквы в виде строки, возможно, 'gbbc';
-// Буквы могут повторяться.
+function getRandomLetters (str, number) {
+    let newStr = '';
+
+    for (let i = 0; i < number; i = i + 1) {
+        const RandomLetters = Math.floor(Math.random() * str.length);
+
+        newStr = newStr + str[RandomLetters];
+    }
+    return newStr;
+}
+console.log(result)
+
 
 // 3. Пишем функцию, которая принимает объект, а возвращает новый
 // объект, в котором только одно случайное свойство из оригинального
 // например для объекта:
-// const person = {
-//   name: 'Vasya',
-//   age: 20,
-//   occupation: 'student',
-//   height: 170,
-// };
 
-// sampleProperty(person) вернет, например,
-// { age: 20 } или { height: 170 }
+const person = {
+  name: 'Vasya',
+  age: 20,
+  occupation: 'student',
+  height: 170,
+};
+
+function sampleProperty (object) {
+    const keyObject = Object.keys(object);
+
+    const index = Math.floor(Math.random() * keyObject.length);
+
+    const value = keyObject[index];
+
+    return { [value]: object[value] };
+}
+
+const randomObject = sampleProperty(person);
+
+console.log(randomObject)
