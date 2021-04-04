11d4ecc2e25c9c907aadc1b33e4d1eada092399c
ValeriiaBond <valeriaredkina19@gmail.com>
2021-04-04 16:14:24 +0300

    hw11 is done

--- homework/s07e01-sample.js
+++ homework/s07e01-sample.js
@@ -1,13 +1,35 @@
 // 1. напишите функцию, которая вернет случайный элемент массива.
 // пример использования:
 
-// const randomColor = sample(['red', 'green', 'blue', 'orange']);
+function getRandomIndex(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function sample(colors) {
+    const randomElement = getRandomIndex(0, colors.length - 1);
+    return colors[randomElement];
+}
+const randomColor = sample(['red', 'green', 'blue', 'orange']);
+console.log(randomColor);
+
 
 // 2. напишите функцию, которая принимает строку и число n, а возвращает
 // новую строку, состоящую из n случайных букв начальной строки.
 // Например
 
-// const result = getRandomLetters('abcdefgh', 4);
+function getRandomLetters(letterArray, lettersAmount) {
+    let randomLetters = '';
+    for (i = 0; i < lettersAmount; i++) {
+        const randomLetterIndex = getRandomIndex(0, letterArray.length - 1);
+        randomLetters = randomLetters + letterArray[randomLetterIndex];
+    }
+    return randomLetters;
+}
+
+const result = getRandomLetters('abcdefgh', 4);
+console.log(result);
 
 // вернет 4 случайно выбранные буквы в виде строки, возможно, 'gbbc';
 // Буквы могут повторяться.
@@ -15,12 +37,21 @@
 // 3. Пишем функцию, которая принимает объект, а возвращает новый
 // объект, в котором только одно случайное свойство из оригинального
 // например для объекта:
-// const person = {
-//   name: 'Vasya',
-//   age: 20,
-//   occupation: 'student',
-//   height: 170,
-// };
+const person = {
+    name: 'Vasya',
+    age: 20,
+    occupation: 'student',
+    height: 170,
+};
 
-// sampleProperty(person) вернет, например,
-// { age: 20 } или { height: 170 }
+function sampleProperty(someObject) {
+    const objectKeys = Object.keys(someObject);
+    const randomIndex = getRandomIndex(0, objectKeys.length - 1);
+    const randomKey = objectKeys[randomIndex];
+
+    const randomeValue = someObject[randomKey];
+
+    return { [randomKey]: randomeValue };
+}
+
+console.log(sampleProperty(person));
--- homework/s07e02-odds.js
+++ homework/s07e02-odds.js
@@ -7,5 +7,17 @@
  * @returns {number[]}
  */
 function getOdds(inputNumbers) {
-  // ВАШ КОД ТУТ
+  const result = [];
+
+  for (element of inputNumbers) {
+    if (element % 2 === 1) {
+      result.push(element);
+    }
+  }
+
+  return result;
 }
+
+console.log(getOdds([2, 4, 5, 6, 7, 8, 9, 22, 44, 3, 57, 3, 2, 12, 21, 11]))
+
+
