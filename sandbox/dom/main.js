6c079eebdc4c32d3533eebf41817a91367f0131d
Dmytro Chyrva <dchyrva413@gmail.com>
2021-04-06 15:03:37 +0300

    Homework is Done

--- sandbox/dom/main.js
+++ sandbox/dom/main.js
@@ -1,27 +1,38 @@
-const GRID_WIDTH = 10;
-const GRID_HEIGHT = 10;
-const CELL_WIDTH_PX = 20;
-const CELL_HEIGHT_PX = 20;
+const GRID_WIDTH = 8;
+const GRID_HEIGHT = 8;
+const CELL_WIDTH_PX = 40;
+const CELL_HEIGHT_PX = 40;
 
 function main() {
   const mainContainer = document.createElement('div');
-  mainContainer.className = 'grid';
+  mainContainer.classList.add('grid');
   mainContainer.style.width = `${GRID_WIDTH * CELL_WIDTH_PX}px`;
-  mainContainer.style.height = `${GRID_HEIGHT * CELL_HEIGHT_PX}px`;
+  mainContainer.style.width = `${GRID_HEIGHT * CELL_HEIGHT_PX}px`;
 
   document.body.appendChild(mainContainer);
 
-  for (let y = 0; y < GRID_WIDTH; y = y + 1) {
-    for (let x = 0; x < GRID_HEIGHT; x = x + 1) {
+  // Creates 8 rows
+  for (let x = 0; x < GRID_WIDTH; x++) {
+    // Creates 8 cells in one row
+    for (let y = 0; y < GRID_HEIGHT; y++) {
+      // Creates a div (cell) element
       const cell = document.createElement('div');
-      cell.classList.add('cell');
+
+      // Gives width and height to the created cell
       cell.style.width = `${CELL_WIDTH_PX}px`;
       cell.style.height = `${CELL_HEIGHT_PX}px`;
 
-      const r = 25 * y;
-      const g = 25 * x;
+      // Check if the row is odd and the cell number is odd
+      if ((x % 2) && (y % 2)) {
+          cell.style.background = 'black';
+      }
 
-      cell.style.backgroundColor = `rgb(${r}, ${g}, 0)`;
+      // For even rows and cells
+      if (!(x % 2) && !(y % 2)) {
+          cell.style.background = 'black';
+      }
+
+      // Appends created div to the main container
       mainContainer.appendChild(cell);
     }
   }
