5578f3c003ef7abafdfe7942fc991fadc69413c3
Yulia Khitrukhina <yulyashenkaa@gmail.com>
2021-04-08 08:11:03 -0700

    uploaded chess board

--- sandbox/dom/main.js
+++ sandbox/dom/main.js
@@ -1,5 +1,5 @@
-const GRID_WIDTH = 10;
-const GRID_HEIGHT = 10;
+const GRID_WIDTH = 8;
+const GRID_HEIGHT = 8;
 const CELL_WIDTH_PX = 20;
 const CELL_HEIGHT_PX = 20;
 
@@ -11,20 +11,25 @@ function main() {
 
   document.body.appendChild(mainContainer);
 
-  for (let y = 0; y < GRID_WIDTH; y = y + 1) {
-    for (let x = 0; x < GRID_HEIGHT; x = x + 1) {
+  for (let x = 0; x < GRID_WIDTH; x = x + 1) {
+    for (let y = 0; y < GRID_HEIGHT; y = y + 1) {
       const cell = document.createElement('div');
       cell.classList.add('cell');
+
       cell.style.width = `${CELL_WIDTH_PX}px`;
       cell.style.height = `${CELL_HEIGHT_PX}px`;
 
-      const r = 25 * y;
-      const g = 25 * x;
-
-      cell.style.backgroundColor = `rgb(${r}, ${g}, 0)`;
+      if ((x % 2) && (y % 2)) {
+        cell.style.background = 'black';
+      } else if (!(x % 2) && !(y % 2)) {
+        cell.style.background = 'black';
+      }
       mainContainer.appendChild(cell);
     }
   }
 }
 
 window.addEventListener('load', main);
+
+
+
