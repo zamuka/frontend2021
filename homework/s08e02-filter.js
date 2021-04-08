e9e625cef9c3acfc54dc24d385fc66ea088c8a95
Kate Gubchenko <porotunya@gmail.com>
2021-04-08 13:52:17 +0300

    add filter hw

--- homework/s08e02-filter.js
+++ homework/s08e02-filter.js
@@ -6,29 +6,36 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 100, 151];
 const evenNumbers = [];
 
-// for (ВАШ) {
-//      КОД
-//      ТУТ
-// }
-
-// [ 2, 4, 6, 8, 100 ]
-console.log(evenNumbers);
+function evenNumbersOfArray(inputArray) {
+  for (let i = 0; i < inputArray.length; i = i + 1) {
+    if (inputArray[i] % 2 === 0) {
+      evenNumbers.push(inputArray[i]);
+    }
+  }
+  return evenNumbers;
+}
+console.log(evenNumbersOfArray(numbers));
 
 // 2. В новом массиве wordsWithO должны быть только слова, в которых есть буква 'o'
 // используйте метод строки - .includes для поиска 'o'
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
-const words = ['grocery', 'sister', 'dirt', 'history', 'opinion', 'payment',
-  'movie', 'drawing', 'area', 'hair', 'selection', 'coffee'];
+
+const words = ['grOcery', 'sister', 'dirt', 'history', 'opinion', 'payment',
+  'movie', 'drawing', 'area', 'Hair', 'selection', 'cOffee'];
 
 const wordsWithO = [];
 
-// for (ВАШ) {
-//      КОД
-//      ТУТ
-// }
+function newArray(inputArray) {
+  for (let i = 0; i < inputArray.length; i = i + 1) {
+    if (inputArray[i].toLowerCase().includes('o')) {
+      wordsWithO.push(inputArray[i]);
+    }
+  }
+  return wordsWithO;
+}
+console.log(newArray(words));
 
-// ['grocery', 'history', 'opinion', 'movie', 'selection', 'coffee']
-console.log(wordsWithO);
+
 
 // В.
 /**
@@ -41,8 +48,27 @@ console.log(wordsWithO);
  * как в конструкции if ()
  */
 
-// [ 2, 4, 6, 8, 100 ]
-console.log(numbers.filter((num) => num/** ваш код тут */));
+// Full record:
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 100, 151];
+const evenNumbers = numbers.filter(function(number) {
+  return number % 2 === 0;
+});
 
-// ['grocery', 'history', 'opinion', 'movie', 'selection', 'coffee']
-console.log(words.filter((word) => word/** ваш код тут */));
+console.log(evenNumbers);
+
+// Arrow function record:
+console.log(numbers.filter((num) => num % 2 === 0));
+
+
+const words = ['grOcery', 'sister', 'dirt', 'history', 'opinion', 'payment',
+  'movie', 'drawing', 'area', 'Hair', 'selection', 'cOffee'];
+let specificLetter = 'o';
+
+// Full record:
+const wordsWithLetter = words.filter(function(item) {
+  return item.toLowerCase().includes(specificLetter);
+});
+console.log(wordsWithLetter);
+
+// Arrow function record:
+console.log(words.filter((word) => word.includes('o')));
