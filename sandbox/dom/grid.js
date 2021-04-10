51914485d2c9b69adf17a24081ce6a645aec3248
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-04-10 10:33:31 +0300

    hw-new_done

--- sandbox/dom/grid.js
+++ sandbox/dom/grid.js
@@ -13,7 +13,7 @@ import {
 function createCell(className = '') {
   const cell = document.createElement('div');
   cell.className = className;
-  cell.classList.add('cell');
+  cell.classList.add('cell', className);
   cell.style.width = `${CELL_WIDTH_PX}px`;
   cell.style.height = `${CELL_HEIGHT_PX}px`;
   return cell;
@@ -27,10 +27,15 @@ const createGrid = function () {
 
   document.body.appendChild(mainContainer);
 
-  for (let y = 0; y < GRID_WIDTH; y = y + 1) {
-    for (let x = 0; x < GRID_HEIGHT; x = x + 1) {
-      const cell = createCell('grass');
-      mainContainer.appendChild(cell);
+  for (let y = 0; y < GRID_HEIGHT; y = y + 1) {
+    for (let x = 0; x < GRID_WIDTH; x = x + 1) {
+      if (x === 0 || y === 0 || y === GRID_HEIGHT - 1 || x === GRID_WIDTH - 1) {
+        const coralCell = createCell('wall');
+        mainContainer.appendChild(coralCell);
+      } else {
+        const cell = createCell('grass');
+        mainContainer.appendChild(cell);
+      }
     }
   }
 };
