a6ab5c4d08a04bf1e763fd3238678673a1cf00af
Dmitry <80134398+DmytroGitAccount@users.noreply.github.com>
2021-04-13 23:02:11 +0300

    Keyboard Event Done

--- sandbox/dom/main.js
+++ sandbox/dom/main.js
@@ -9,17 +9,62 @@ const snake = {
     { x: 6, y: 5 },
     { x: 5, y: 5 },
   ],
+  direction: '',
 };
+
+/**
+ * Changes direction depending on the pressed key
+ * @param {object} event
+ */
+function changeDirection(event){
+  switch(event.keyCode){
+    case 40:
+      if(snake.direction !== "Up"){
+        snake.direction = 'Down';
+      }
+      break;
+    case 38:
+      if(snake.direction !== "Down"){
+        snake.direction = 'Up';
+      }
+      break;
+    case 37:
+      if(snake.direction !== "Right"){
+        snake.direction = 'Left';
+      }
+      break;
+    case 39:
+      if(snake.direction !== "Left"){
+        snake.direction = 'Right';
+      }
+      break;
+  }
+}
+/**
+ * Changes Snake Head Cordinates
+ * @param {object} element object with coordinates
+ * @param {string} direction string wiht direction pointer
+ */
+function setDirection(element, direction){
+  switch(direction){
+    case 'Down':
+      return element.y = element.y + 1;
+    case "Up":
+      return element.y = element.y - 1;
+    case "Left":
+      return element.x = element.x - 1;
+    case "Right":
+      return element.x = element.x + 1;
+    default:
+      return element.x = element.x + 1;
+  }
+}
 
 function doStep() {
   const currentHead = snake.cells[0];
-  // const newHead = Object.assign({}, currentHead);
-  // const newHead = {
-  //   x: currentHead.x,
-  //   y: currentHead.y,
-  // };
   const newHead = { ...currentHead };
-  newHead.x = newHead.x + 1;
+
+  setDirection(newHead, snake.direction);
 
   snake.cells.unshift(newHead);
   setCellClass(newHead.x, newHead.y, CELL_TYPES.SNAKE);
@@ -35,7 +80,8 @@ function main() {
     setCellClass(cell.x, cell.y, CELL_TYPES.SNAKE);
   });
 
-  setInterval(doStep, 1000);
+  setInterval(doStep, 100);
 }
 
 window.addEventListener('load', main);
+window.addEventListener('keydown', changeDirection);
