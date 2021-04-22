ceda0c4f557830ba9d6be320563ec49116417979
Dmytro <dchyrva413@gmail.com>
2021-04-22 16:06:54 +0300

    HW Odds Cleaned

--- homework/s07e02-odds.js
+++ homework/s07e02-odds.js
@@ -9,10 +9,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 function getOdds(inputNumbers) {
-  // ВАШ КОД ТУТ
   const result = [];
 
-  // eslint-disable-next-line no-restricted-syntax
   for (const number of inputNumbers) {
     if (number % 2 !== 0) {
       result.push(number);
