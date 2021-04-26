494a979f3afa310d5d554bfa56ad16bb096da7be
Sergey Zamoshnikov <zamukan@gmail.com>
2021-04-26 22:23:14 +0300

    cw15

---
+++ sandbox/farm/animals/chicken.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-unused-vars
+import { CanvasLib } from '../../libs/canvas-lib.js';
+import { baseAnimal } from './animal.js';
+
+/**
+ * @param {CanvasLib} canvas
+ */
+function Chicken(canvas) {
+  /**
+   * this = {
+   *   __proto__: Chicken.prototype,
+   * }
+   */
+
+  this.pixels = [
+    { x: -1, y: -2 }, { x: -2, y: -1 }, { x: -1, y: -1 }, { x: -1, y: 0 },
+    { x: 0, y: 0 }, { x: 1, y: 0 }, { x: -1, y: 1 }, { x: 0, y: 1 },
+    { x: 1, y: 1 }, { x: 0, y: 2 }, { x: 2, y: -1 },
+  ];
+
+  // @ts-ignore
+  this.init(canvas);
+}
+
+Chicken.prototype = baseAnimal;
+
+export {
+  Chicken,
+};
--- sandbox/farm/main.js
+++ sandbox/farm/main.js
@@ -1,56 +1,35 @@
 import { CanvasLib } from '../libs/canvas-lib.js';
+import {
+  Chicken,
+  Cow,
+} from './animals/index.js';
+
+const CHICKEN_COUNT = 10;
+const COW_COUNT = 3;
+
+let canvas = null;
+const animals = [];
+
+function gameStep() {
+  canvas.clear('green');
+  canvas.setColor('white');
+  animals.forEach((animal) => animal.move());
+  animals.forEach((animal) => animal.draw());
+}
 
 function startUp() {
-  const canvas = new CanvasLib('canvas');
+  canvas = new CanvasLib('canvas');
 
   canvas.init({ zoom: 5 });
-  canvas.clear('green');
 
-  canvas.setColor('white');
-
-  function createChicken(startX, startY) {
-    const chicken = {
-      x: startX,
-      y: startY,
-      pixels: [
-        { x: -1, y: -2 }, { x: -2, y: -1 }, { x: -1, y: -1 }, { x: -1, y: 0 },
-        { x: 0, y: 0 }, { x: 1, y: 0 }, { x: -1, y: 1 }, { x: 0, y: 1 },
-        { x: 1, y: 1 }, { x: 0, y: 2 }, { x: 2, y: -1 },
-      ],
-
-      draw() {
-        this.pixels
-          .forEach(({ x, y }) => canvas.setPixel(this.x + x, this.y + y));
-      },
-    };
-    return chicken;
+  for (let i = 0; i < CHICKEN_COUNT; i = i + 1) {
+    animals.push(new Chicken(canvas));
+  }
+  for (let i = 0; i < COW_COUNT; i = i + 1) {
+    animals.push(new Cow(canvas));
   }
 
-  // почти то же самое, но с помощью функции-контруктора
-  function Chicken(startX, startY) {
-    const pixels = [
-      { x: -1, y: -2 }, { x: -2, y: -1 }, { x: -1, y: -1 }, { x: -1, y: 0 },
-      { x: 0, y: 0 }, { x: 1, y: 0 }, { x: -1, y: 1 }, { x: 0, y: 1 },
-      { x: 1, y: 1 }, { x: 0, y: 2 }, { x: 2, y: -1 },
-    ];
-
-    this.x = startX;
-    this.y = startY;
-
-    this.draw = function () {
-      pixels.forEach(({ x, y }) => canvas.setPixel(this.x + x, this.y + y));
-    };
-  }
-
-  const chicken = new Chicken(130, 65);
-
-  const animals = [
-    chicken,
-    new Chicken(120, 60),
-    createChicken(110, 70),
-  ];
-
-  animals.forEach((animal) => animal.draw());
+  setInterval(gameStep, 50);
 }
 
 window.addEventListener('load', startUp);
