d172662323f9a087a6d9ccaaa39c342b6de25420
InnaStanislavskaya <stanislavskaya.i.b@gmail.com>
2021-04-30 22:45:45 +0300

    remade her rabbit using a new syntax - classes.

--- sandbox/farm/main.js
+++ sandbox/farm/main.js
@@ -4,12 +4,14 @@ import {
   Cow,
   Pig,
   Duck,
+  Rabbit,
 } from './animals/index.js';
 
 const CHICKEN_COUNT = 10;
 const COW_COUNT = 3;
 const PIG_COUNT = 5;
 const DUCK_COUNT = 7;
+const RABBIT_COUNT = 5;
 
 let canvas = null;
 const animals = [];
@@ -38,6 +40,10 @@ function startUp() {
 
   for (let i = 0; i < DUCK_COUNT; i = i + 1) {
     animals.push(new Duck(canvas));
+  }
+
+  for (let i = 0; i < RABBIT_COUNT; i = i + 1) {
+    animals.push(new Rabbit(canvas));
   }
 
   setInterval(gameStep, 50);
