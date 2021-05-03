96f0edcf86dbe519b12daa3dd7e8c46cd4b7f6f2
GulenkoAnastasia <nansi0307@yahoo.com>
2021-05-03 11:55:12 +0300

    the goats are added using classes

--- sandbox/farm/animals/index.js
+++ sandbox/farm/animals/index.js
@@ -2,5 +2,6 @@ export * from './chicken.js';
 export * from './cow.js';
 export * from './duck.js';
 export * from './animal.js';
+export * from './goat.js';
 export * from './pig.js';
 export * from './rabbit.js';
