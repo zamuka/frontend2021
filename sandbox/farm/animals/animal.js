684eabdf381077f1f23580da3acbfbbcadb478e8
sveta.zaletskaya <lana.zaletskaya@gmail.com>
2021-05-12 23:42:17 +0300

    farm proto

--- sandbox/farm/animals/animal.js
+++ sandbox/farm/animals/animal.js
@@ -1,28 +1,31 @@
 function Animal() {
-  this._canvas = null;
-  this.pixels = [];
-  this.color = 'white';
+    this._canvas = null;
+    this.pixels = [];
+    this.color = 'white';
 }
 
 Animal.prototype.init = function (canvas) {
-  this._canvas = canvas;
-  this.x = Math.floor(Math.random() * this._canvas.width);
-  this.y = Math.floor(Math.random() * this._canvas.height);
+    this._canvas = canvas;
+    this.x = Math.floor(Math.random() * this._canvas.width);
+    this.y = Math.floor(Math.random() * this._canvas.height);
 };
 
 Animal.prototype.draw = function () {
-  this._canvas.setColor(this.color);
-  this.pixels.forEach(({ x, y }) => this._canvas.setPixel(this.x + x, this.y + y));
+    this._canvas.setColor(this.color);
+    this.pixels.forEach(({x, y, color}) => {
+        this._canvas.setPixel(this.x + x, this.y + y);
+        this._canvas.setColor(color);
+    });
 };
 
 Animal.prototype.move = function () {
-  this.x = this.x + Math.random() - 0.5;
-  this.y = this.y + Math.random() - 0.5;
+    this.x = this.x + Math.random() - 0.5;
+    this.y = this.y + Math.random() - 0.5;
 };
 
 const baseAnimal = new Animal();
 
 export {
-  Animal,
-  baseAnimal,
+    Animal,
+    baseAnimal,
 };
