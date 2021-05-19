4e366847ea9163eb7dd43895db7677fe5a6a8a50
Vlada-khom <vvlada2610@gmail.com>
2021-05-19 13:20:24 +0300

    hw 27-server(error)

--- classwork/s18e01-tcp-server.js
+++ classwork/s18e01-tcp-server.js
@@ -13,6 +13,14 @@ server.on('connection', function (socket) {
     const [reqLine, otherLines] = reqText.split('\n');
     const [method, path, protocol] = reqLine.split(' ');
 
+    if (method !== 'GET') {
+      socket.write('HTTP/ 1.1 400 Bad Request\n');
+      socket.write('\n');
+      socket.write('<html><body><h1>ERROR 400</h1></body></html>\n');
+      socket.end();
+      return;
+    }
+
     if (path === '/favicon.ico') {
       socket.write('HTTP/1.1 200 FINE\n');
       socket.write('\n'); // пустая строка
@@ -22,11 +30,20 @@ server.on('connection', function (socket) {
       return;
     }
 
+    if (path === '/') {
+      socket.write('HTTP/1.1 200 FINE\n');
+      socket.write('\n'); // пустая строка
+      socket.write('<html><body><h1>Hello world</h1></body></html>\n');
+
+      socket.end();
+      return;
+    }
+
     console.log(reqLine);
 
-    socket.write('HTTP/1.1 200 OK\n');
+    socket.write('HTTP/1.1 404 Not Found\n');
     socket.write('\n'); // пустая строка
-    socket.write('<html><body><h1>Hello world</h1></body></html>\n');
+    socket.write(`<html><body><h1>Error 404. \n URL ${path} was not found.</h1></body></html>\n`);
 
     socket.end();
   });
