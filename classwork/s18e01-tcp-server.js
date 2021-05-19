1d353cd2a97a72a440b3df37c4e141456c7f331a
GulenkoAnastasia <nansi0307@yahoo.com>
2021-05-19 13:17:36 +0300

    the order of conditions is changed

--- classwork/s18e01-tcp-server.js
+++ classwork/s18e01-tcp-server.js
@@ -13,6 +13,15 @@ server.on('connection', function (socket) {
     const [reqLine, otherLines] = reqText.split('\n');
     const [method, path, protocol] = reqLine.split(' ');
 
+    if (method !== 'GET') {
+      socket.write('HTTP/1.1 400 Bad Request\n');
+      socket.write('\n');
+      socket.write('<html><body><h1>Error 400. Bad Request</h1></body></html>\n');
+
+      socket.end();
+      return;
+    }
+
     if (path === '/favicon.ico') {
       socket.write('HTTP/1.1 200 FINE\n');
       socket.write('\n'); // пустая строка
@@ -26,14 +35,6 @@ server.on('connection', function (socket) {
       socket.write('HTTP/1.1 200 OK\n');
       socket.write('\n');
       socket.write('<html><body><h1>Hello world</h1></body></html>\n');
-
-      socket.end();
-      return;
-    }
-
-    if (method !== 'GET') {
-      socket.write('HTTP/1.1 400 Bad Request\n');
-      socket.write('\n');
 
       socket.end();
       return;
