f0919b53222335f0573f89ce128833dd5f7ce466
Dmytro <dchyrva413@gmail.com>
2021-05-26 21:07:36 +0300

    findid_method_done

--- classwork/server/yourNoSql.js
+++ classwork/server/yourNoSql.js
@@ -7,6 +7,10 @@ class YourNoSql {
   getList() {
     return JSON.parse(fs.readFileSync(this.dataFileName, 'utf-8'));
   }
+
+  findId(id) {
+    return this.getList().find((item) => id === item._id);
+  }
 }
 
 module.exports = {
