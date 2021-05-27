26ab90657d9745a22b429721ce874523851383c5
Anastasia Gulenko <nansi0307@yahoo.com>
2021-05-27 19:45:32 +0300

    HW_29 is done.

--- classwork/server/main.js
+++ classwork/server/main.js
@@ -1,13 +1,16 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const url = require('url');
+const queryString = require('querystring');
 const { dbClient } = require('./yourNoSql');
 const { serveStatic } = require('./serveStatic');
 const Mustache = require('mustache');
 
 const templates = {
   userList: fs.readFileSync(path.join(__dirname, 'templates/userList.html'), 'utf-8'),
-}
+  selectedUser: fs.readFileSync(path.join(__dirname, 'templates/selectedUser.html'), 'utf-8'),
+};
 
 /**
  * @param {http.IncomingMessage} req
@@ -27,8 +30,44 @@ function listener(req, res) {
     const content = Mustache.render(templates.userList, { title: 'User List from data', users });
     res.write(content);
     res.end();
-
   }
+
+  if (req.url.startsWith('/users/')) {
+    res.statusCode = 200;
+    let id = req.url.slice(7);
+    let user = dbClient.findId(id);
+    let urlArguments = url.parse(req.url).search;
+
+    if (user) {
+      const content = Mustache.render(templates.selectedUser, { title: `${user.name}`, user });
+      res.write(content);
+      res.end();
+      return;
+    }
+
+    if (urlArguments) {
+      id = id.slice(0, -urlArguments.length);
+      user = dbClient.findId(id);
+      urlArguments = urlArguments.slice(1);
+      urlArguments = queryString.parse(urlArguments);
+
+      user.name = urlArguments.name;
+      user.balance = urlArguments.balance;
+      dbClient.updateUser(id, user);
+
+      res.statusCode = 301;
+      res.writeHead(302, {
+        Location: '/users.html',
+      });
+      res.end();
+      return;
+    }
+
+    res.statusCode = 404;
+    res.write('Sorry, this ID does not exist. Enter the correct ID.');
+    res.end();
+  }
+
   serveStatic(req, res);
 }
 
--- classwork/server/yourNoSql.js
+++ classwork/server/yourNoSql.js
@@ -7,6 +7,22 @@ class YourNoSql {
   getList() {
     return JSON.parse(fs.readFileSync(this.dataFileName, 'utf-8'));
   }
+
+  findId(id) {
+    const users = this.getList();
+    return users.find((user) => user._id === id);
+  }
+
+  updateUser(id, newUser) {
+    const originalUsers = this.getList();
+    const users = originalUsers.map(function (user) {
+      if (user._id === id) {
+        return newUser;
+      }
+      return user;
+    });
+    fs.writeFileSync(this.dataFileName, JSON.stringify(users, null, 2));
+  }
 }
 
 module.exports = {
