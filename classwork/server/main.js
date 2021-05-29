38c585d35777bb61e8727508c4b6ba832e33b8e8
Sergey Zamoshnikov <zamukan@gmail.com>
2021-05-29 12:20:21 +0300

    cw21

--- classwork/server/main.js
+++ classwork/server/main.js
@@ -1,13 +1,15 @@
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
+const Mustache = require('mustache');
+const { isEmpty } = require('lodash');
 const { dbClient } = require('./yourNoSql');
 const { serveStatic } = require('./serveStatic');
-const Mustache = require('mustache');
 
 const templates = {
   userList: fs.readFileSync(path.join(__dirname, 'templates/userList.html'), 'utf-8'),
-}
+  user: fs.readFileSync(path.join(__dirname, 'templates/user.html'), 'utf-8'),
+};
 
 /**
  * @param {http.IncomingMessage} req
@@ -20,6 +22,14 @@ function listener(req, res) {
     return;
   }
 
+  if (req.url === '/' || req.url === '/users' || req.url === '/users/') {
+    res.writeHead(302, {
+      Location: '/users.html',
+    });
+    res.end();
+    return;
+  }
+
   if (req.url === '/users.html') {
     const users = dbClient.getList();
     res.statusCode = 200;
@@ -27,8 +37,34 @@ function listener(req, res) {
     const content = Mustache.render(templates.userList, { title: 'User List from data', users });
     res.write(content);
     res.end();
-
+    return;
   }
+
+  if (req.url.startsWith('/users/')) {
+    const baseURL = `http://${req.headers.host}/`;
+    const myURL = new URL(req.url, baseURL);
+    const [,, id] = myURL.pathname.split('/');
+
+    const userUpdateData = Object.fromEntries(myURL.searchParams.entries());
+
+    if (isEmpty(myURL.search)) {
+      const user = dbClient.findUser(id);
+      const content = Mustache.render(templates.user, user);
+      res.write(content);
+      res.end();
+      return;
+    }
+
+    dbClient.update(id, userUpdateData, () => {
+      res.writeHead(302, {
+        Location: '/users.html',
+      });
+      res.end();
+    });
+
+    return;
+  }
+
   serveStatic(req, res);
 }
 
--- classwork/server/yourNoSql.js
+++ classwork/server/yourNoSql.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const { find } = require('lodash');
 const path = require('path');
 
 class YourNoSql {
@@ -6,6 +7,29 @@ class YourNoSql {
 
   getList() {
     return JSON.parse(fs.readFileSync(this.dataFileName, 'utf-8'));
+  }
+
+  findUser(id) {
+    return find(this.getList(), { _id: id });
+  }
+
+  update(id, userData, cb) {
+    const originalUsers = this.getList();
+    const users = originalUsers.map((user) => {
+      if (user._id === id) {
+        return {
+          ...user,
+          ...userData,
+        };
+      }
+      return user;
+    });
+
+    this.save(users, cb);
+  }
+
+  save(data, cb) {
+    fs.writeFile(this.dataFileName, JSON.stringify(data), cb);
   }
 }
 
