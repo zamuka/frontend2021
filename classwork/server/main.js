3fc5e1b004e63456780f77b972b4dd5e9a9e3e07
Yulia Khitrukhina <yulyashenkaa@gmail.com>
2021-05-31 12:30:54 -0700

    hw_29

--- classwork/server/main.js
+++ classwork/server/main.js
@@ -1,33 +1,53 @@
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
-const { dbClient } = require('./yourNoSql');
-const { serveStatic } = require('./serveStatic');
-const Mustache = require('mustache');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+const { dbClient } = require("./yourNoSql");
+const { serveStatic } = require("./serveStatic");
+const Mustache = require("mustache");
 
 const templates = {
-  userList: fs.readFileSync(path.join(__dirname, 'templates/userList.html'), 'utf-8'),
-}
+  userList: fs.readFileSync(
+    path.join(__dirname, "templates/userList.html"),
+    "utf-8"
+  ),
+  userSelected: fs.readFileSync(
+    path.join(__dirname, "templates/userSelected.html"),
+    "utf-8"
+  ),
+};
 
 /**
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
  */
 function listener(req, res) {
-  if (req.method !== 'GET') {
+  if (req.method !== "GET") {
     res.statusCode = 405;
     res.end();
     return;
   }
 
-  if (req.url === '/users.html') {
+  if (req.url === "/users.html") {
     const users = dbClient.getList();
     res.statusCode = 200;
 
-    const content = Mustache.render(templates.userList, { title: 'User List from data', users });
+    const content = Mustache.render(templates.userList, {
+      title: "User List from data",
+      users,
+    });
     res.write(content);
     res.end();
+  }
 
+  if (req.url.startsWith("/users/")) {
+    res.statusCode = 200;
+    const id = req.url.slice(7);
+    let user = dbClient.find(id);
+    if (user) {
+      let content = Mustache.render(templates.userSelected, dbClient.find(id));
+      res.write(content);
+      res.end();
+    }
   }
   serveStatic(req, res);
 }
--- classwork/server/yourNoSql.js
+++ classwork/server/yourNoSql.js
@@ -1,11 +1,16 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 class YourNoSql {
-  dataFileName = path.join(__dirname, 'users.json');
+  dataFileName = path.join(__dirname, "users.json");
 
   getList() {
-    return JSON.parse(fs.readFileSync(this.dataFileName, 'utf-8'));
+    return JSON.parse(fs.readFileSync(this.dataFileName, "utf-8"));
+  }
+
+  find(id) {
+    let users = this.getList();
+    return users.find((item) => id === item._id);
   }
 }
 
