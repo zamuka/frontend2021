a20635dbd4e4a0e0026dcb95b314fd80647af7d2
Sergey Zamoshnikov <zamukan@gmail.com>
2021-06-01 10:36:09 +0300

    cw22

--- classwork/server/yourNoSql.js
+++ classwork/server/yourNoSql.js
@@ -9,6 +9,15 @@ class YourNoSql {
     return JSON.parse(fs.readFileSync(this.dataFileName, 'utf-8'));
   }
 
+  getListAsPromised() {
+    return new Promise((resolve) => {
+      fs.readFile(this.dataFileName, 'utf-8', (err, data) => {
+        const list = JSON.parse(data);
+        resolve(list);
+      });
+    });
+  }
+
   findUser(id) {
     return find(this.getList(), { _id: id });
   }
