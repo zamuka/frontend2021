4423d3d6b2396f7ea37f5aed701ea216cd22cdc2
Alexey Popov <alexeybarmenn@gmail.com>
2021-06-03 11:59:32 +0300

    traffic-lights_v1

--- sandbox/traffic-light/traffic-light.js
+++ sandbox/traffic-light/traffic-light.js
@@ -1,3 +1,4 @@
+
 /**
  * Светофоры находятся в полном беспорядке.
  * Ваша здача починить их и заставить работать как надо с помощью
@@ -19,5 +20,57 @@
  * 3. Добавьте жетлый вместе с красным перед зеленым
  * 4. Обратный отсчет - конечно, задание со звездочкой
  */
+const lights = document.querySelectorAll('.light');
+const arrayLights = Array.from(lights);
+let delaySec = 10;
 
-/** YOUR CODE HERE */
+
+function changeColor () {
+  
+  arrayLights.forEach((elem, index) => {
+    
+    if (index === 1) {
+      elem.classList.add("red");
+      
+    }
+    if (index === 2) {
+      elem.classList.remove("red");
+      elem.classList.add("green")
+      elem.classList.remove("yellow");
+    }
+    yellowLight(elem, index);
+  });
+  
+    setInterval(() => {
+      arrayLights.forEach((elem, index) => {
+        elem.classList.toggle("red");
+        elem.classList.toggle("green");
+        yellowLight(elem, index);
+        
+      })
+    }, ((delaySec + 3) * 1000))
+    
+}
+
+function yellowLight (elem, index) {
+  if (index === 1 || index === 2) {
+          setTimeout(() => {
+            elem.classList.toggle("yellow");
+            let intervalId = setInterval(() => {
+              elem.classList.toggle("yellow");
+            }, ((delaySec - 7) * 1000 / 8))
+            setTimeout(() => {
+              clearInterval(intervalId)
+              elem.classList.add('yellow');
+            }, ((delaySec - 7) * 1000));
+            setTimeout( () => {
+              elem.classList.remove('yellow');
+            }, 6000)
+          }, ((delaySec - 3) * 1000));
+        }
+}
+
+
+
+  
+  document.addEventListener('load', changeColor())
