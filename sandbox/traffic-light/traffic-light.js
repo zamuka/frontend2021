753984e3a6c5458246d3b4d70fcce685341fccbe
Yevhen <zhekahome@gmail.com>
2021-06-03 15:21:38 +0300

    HW31 is done

--- sandbox/traffic-light/traffic-light.js
+++ sandbox/traffic-light/traffic-light.js
@@ -21,3 +21,87 @@
  */
 
 /** YOUR CODE HERE */
+
+function changeColor() {
+  setTimeout(
+    function () {
+      carLeftLight.classList.add('red')
+      carLeftLight.classList.remove('yellow')
+      carRightLight.classList.add('green')
+      carRightLight.classList.remove('red', 'yellow')
+      footerLeftLight.classList.add('green')
+      footerLeftLight.classList.remove('red')
+      footerRightLight.classList.add('red')
+      footerRightLight.classList.remove('green')
+      setTimeout(
+        function () {
+          carLeftLight.classList.add('yellow')
+          setTimeout (
+            function () {
+              carRightLight.classList.add('yellow')
+              carRightLight.classList.remove('green')
+              setTimeout (
+                function () {
+                  carLeftLight.classList.remove('yellow', 'red')
+                  carLeftLight.classList.add('green')
+                  carRightLight.classList.remove('yellow')
+                  carRightLight.classList.add('red')
+                  footerLeftLight.classList.add('red')
+                  footerLeftLight.classList.remove('green')
+                  footerRightLight.classList.add('green')
+                  footerRightLight.classList.remove('red')
+                  setTimeout (
+                    function () {
+                      carRightLight.classList.add('yellow')
+                      setTimeout (
+                        function () {
+                          carLeftLight.classList.add('yellow')
+                          carLeftLight.classList.remove('green')
+                          changeColor();
+                        }, 1000
+                      )
+                    },10000
+                  )
+                }, 1000
+              )
+            }, 1000
+          ) 
+        }, 10000
+      )
+    }, 1000
+  )
+};
+
+
+function timer() {
+  if (time === 1) {
+    time = 13;
+  }
+  setTimeout (
+    function () {
+      time = time - 1;
+      timerLeftFooterGreen.innerHTML = time;
+      timerRightFooterGreen.innerHTML = time;
+      timerLeftFooterRed.innerHTML = time;
+      timerRightFooterRed.innerHTML = time;
+      timer();
+    }, 1000
+  )
+}
+
+
+function startUp() {
+  changeColor();
+  timer();
+}
+
+window.addEventListener('load', startUp);
+const carRightLight = document.getElementById('carRightLight');
+const carLeftLight = document.getElementById('carLeftLight');
+const footerRightLight = document.getElementById('footerRightLight');
+const footerLeftLight = document.getElementById('footerLeftLight');
+const timerLeftFooterGreen = document.querySelector('#footerLeftLight .green');
+const timerRightFooterGreen = document.querySelector('#footerRightLight .green');
+const timerLeftFooterRed = document.querySelector('#footerLeftLight .red');
+const timerRightFooterRed = document.querySelector('#footerRightLight .red');
+let time = 13;
