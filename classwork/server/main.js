48c141780ec9010d3377e22e0d5f036855a69844
Yulia Khitrukhina <yulyashenkaa@gmail.com>
2021-06-10 06:11:09 +0300

    hw 32 - rewritten using promises

--- classwork/server/main.js
+++ classwork/server/main.js
@@ -1,74 +1,131 @@
-const http = require("http");
-const path = require("path");
-const fs = require("fs");
-const { dbClient } = require("./yourNoSql");
-const { serveStatic } = require("./serveStatic");
-const Mustache = require("mustache");
+const http = require('http');
+const path = require('path');
+// const fs = require('fs');
+const Mustache = require('mustache');
+const { isEmpty } = require('lodash');
+const { dbClient } = require('./yourNoSql');
+const { serveStatic } = require('./serveStatic');
+const fsp = require('fs/promises');
 
-const templates = {
-  userList: "",
-  userSelected: "",
+const templateList = {
+  userList: {
+    content: null,
+    fileName: path.join(__dirname, 'templates/userList.html'),
+  },
+  user: {
+    content: null,
+    fileName: path.join(__dirname, 'templates/user.html'),
+  },
 };
-
-fs.readFile(
-  path.join(__dirname, "templates/userList.html"),
-  "utf-8",
-  (err, data) => {
-    if (err) throw err;
-    templates.userList = data;
-  }
-);
-fs.readFile(
-  path.join(__dirname, "templates/userSelected.html"),
-  "utf-8",
-  (err, data) => {
-    if (err) throw err;
-    templates.userSelected = data;
-  }
-);
 
 /**
  * @param {http.IncomingMessage} req
  * @param {http.ServerResponse} res
  */
 function listener(req, res) {
-  if (req.method !== "GET") {
+  if (req.method !== 'GET') {
     res.statusCode = 405;
     res.end();
     return;
   }
 
-  if (req.url === "/users.html") {
-    const users = dbClient.getList();
-    res.statusCode = 200;
-
-    const content = Mustache.render(templates.userList, {
-      title: "User List from data",
-      users,
+  if (req.url === '/' || req.url === '/users' || req.url === '/users/') {
+    res.writeHead(302, {
+      Location: '/users.html',
     });
-    res.write(content);
     res.end();
+    return;
   }
 
-  if (req.url.startsWith("/users/")) {
-    res.statusCode = 200;
-    const id = req.url.slice(7);
-    let user = dbClient.find(id);
-    if (user) {
-      let content = Mustache.render(templates.userSelected, dbClient.find(id));
+  if (req.url === '/users.html') {
+    const usersPromise = dbClient.getList();
+
+    // pending
+    // resolved
+    // rejected
+
+    usersPromise.then(function (users) {
+      res.statusCode = 200;
+      const content = Mustache.render(templateList.userList.content, {
+        title: 'User List from data',
+        users,
+      });
       res.write(content);
       res.end();
-    }
+    });
+
+    return;
   }
+
+  if (req.url.startsWith('/users/')) {
+    const baseURL = `http://${req.headers.host}/`;
+    const myURL = new URL(req.url, baseURL);
+    const [, , id] = myURL.pathname.split('/');
+
+    const userUpdateData = Object.fromEntries(myURL.searchParams.entries());
+
+    if (isEmpty(myURL.search)) {
+      dbClient.findUser(id).then((user) => {
+        const content = Mustache.render(templateList.user.content, user);
+        res.write(content);
+        res.end();
+      });
+      return;
+    }
+
+    dbClient.update(id, userUpdateData).then(() => {
+      res.writeHead(302, {
+        Location: '/users.html',
+      });
+      res.end();
+    });
+    return;
+  }
+
   serveStatic(req, res);
 }
 
-let timer = setInterval(checkTemplates, 1);
+const server = http.createServer(listener);
 
-function checkTemplates() {
-  if (templates.userList !== "" && templates.userSelected !== "") {
-    const server = http.createServer(listener);
-    server.listen(9090);
-    clearInterval(timer);
-  }
-}
+let promises = [];
+
+//1
+Object.entries(templateList).forEach(([templateName, template]) => {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(template.fileName, 'utf-8', (error, userListContent) => {
+      resolve(userListContent);
+      if (error) {
+        reject('Error!', error);
+      }
+    });
+  });
+  promise
+    .then(
+      (userListContent) =>
+        (templateList[templateName].content = userListContent)
+    )
+    .then(() => promises.push(promise))
+    .catch((error) => {
+      console.log('Error', error);
+    });
+});
+
+Promise.all(promises).then(function () {
+  server.listen(9080);
+});
+
+//2
+Object.entries(templateList).forEach(([templateName, template]) => {
+  let promise = fsp
+    .readFile(template.fileName, 'utf8')
+    .then(
+      (userListContent) =>
+        (templateList[templateName].content = userListContent)
+    )
+    .then(() => promises.push(promise))
+    .catch((error) => console.log('Error', error));
+});
+
+Promise.all(promises).then(function () {
+  server.listen(9080);
+});
