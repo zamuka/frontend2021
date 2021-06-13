87fbd08ae4a9f2224fd8a3488307a96f26aa5175
Dmitry <80134398+dmytrochyrva@users.noreply.github.com>
2021-06-13 14:52:14 +0300

    form_validation

--- classwork/server/public/scripts/editform.js
+++ classwork/server/public/scripts/editform.js
@@ -1,1 +1,46 @@
-console.log('HELLO');
+class FormValidation {
+  constructor(node) {
+    this.node = node;
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  preventSubmit(event) {
+    // eslint-disable-next-line class-methods-use-this
+    event.preventDefault();
+  }
+
+  blockFormSubmit() {
+    this.node.addEventListener(
+      'submit',
+      this.preventSubmit,
+    );
+  }
+
+  allowFormSubmit() {
+    this.node.removeEventListener(
+      'submit',
+      this.preventSubmit,
+    );
+  }
+
+  isEmptyField() {
+    this.node.addEventListener('input', (event) => {
+      if (event.target.value === '') {
+        event.target.classList.add('is-invalid');
+        this.blockFormSubmit();
+        return;
+      }
+
+      event.target.classList.remove('is-invalid');
+      this.allowFormSubmit();
+    });
+  }
+}
+
+function onStartUp() {
+  const form = new FormValidation(document.querySelector('#edit-user-info'));
+
+  form.isEmptyField();
+}
+
+window.addEventListener('DOMContentLoaded', onStartUp);
