9f3302397bea003dadaa34ee0d0076ccb69febb7
Anastasia Gulenko <nansi0307@yahoo.com>
2021-06-13 15:50:49 +0300

    Homework 35 is done

--- classwork/server/public/scripts/editform.js
+++ classwork/server/public/scripts/editform.js
@@ -1,1 +1,33 @@
-console.log('HELLO');
+function formValidation() {
+  const form = document.querySelector('form');
+  const inputName = form.querySelector('.name');
+  const inputGender = form.querySelector('.gender');
+  const allInputs = Array.from(form.querySelectorAll('.form-control'));
+
+  form.addEventListener('submit', async function (event) {
+    event.preventDefault();
+    const regExpForName = /^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)/;
+    const emptyElement = allInputs.find((input) => !input.value);
+    if (emptyElement) {
+      emptyElement.classList.add('is-invalid');
+      emptyElement.placeholder = 'This input field shouldn\'t be empty';
+      return;
+    }
+    if (!regExpForName.test(inputName.value)) {
+      inputName.classList.add('is-invalid');
+      return;
+    }
+
+    if (inputGender.value !== 'female' && inputGender.value !== 'male') {
+      inputGender.classList.add('is-invalid');
+      return;
+    }
+
+    const search = `?name=${inputName.value}&gender=${inputGender.value}`;
+
+    await fetch(`${window.location.pathname}${search}`);
+    window.location.pathname = '/users.html';
+  });
+}
+
+window.addEventListener('load', formValidation);
