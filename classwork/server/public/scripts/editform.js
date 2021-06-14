f929ffced7a99eac41e7ca005c4083caf5c2960d
Alexey Popov <alexeybarmenn@gmail.com>
2021-06-14 12:27:29 +0300

    HW-35

--- classwork/server/public/scripts/editform.js
+++ classwork/server/public/scripts/editform.js
@@ -1,1 +1,43 @@
-console.log('HELLO');
+function main() {
+  const form = document.querySelector('form');
+  const userName = document.querySelector('[name="name"]');
+  const userGender = document.querySelector('[name="gender"]');
+  const arrInputs = Array.from(form.querySelectorAll('.form-control'));
+
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const volidName = /^([A-Za-z])/;
+
+    arrInputs.forEach((element) => {
+
+      if (!element.value) {
+        element.classList.add('is-invalid');
+        element.placeholder = 'This is not volid'
+        return
+      }
+
+      if (element === userName) {
+        if (!volidName.test(userName.value)) {
+          element.classList.add('is-invalid');
+          alert(`Your name isn't volid`)
+          return
+        }
+      }
+
+      if (element === userGender) {
+        if (userGender.value !== 'female' && userGender.value !== 'male') {
+        element.classList.add('is-invalid');
+        alert(`Your gender isn't volid`)
+        return
+      }
+      }
+      if (element.value) {
+        element.classList.remove('is-invalid');
+      }
+    })
+    
+  });
+}
+
+window.addEventListener('load', main);
