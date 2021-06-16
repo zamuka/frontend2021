4310a159f85195a86b5a1e24d9010f803103a3a1
Yulia Khitrukhina <yulyashenkaa@gmail.com>
2021-06-16 21:22:00 +0300

    User form validation

--- classwork/server/public/scripts/editform.js
+++ classwork/server/public/scripts/editform.js
@@ -1,19 +1,47 @@
-/**
- *
- * @param {Event} event
- */
-const handleFormSubmit = async (event) => {
-  event.preventDefault();
+function isFormValid() {
+  const form = document.querySelector('form');
+  const nameInput = form.querySelector('input[name=name]');
+  const genderInput = form.querySelector('input[name=gender]');
+  const nameInvalidFeedback = document.querySelector('#invalid-feedback-name');
+  const inputs = form.querySelectorAll('.form-control');
+  const valid_pattern = /^([a-zA-Z])/;
 
-  const id = window.location.pathname.split('/').pop();
+  function addInvalidClass(element) {
+    element.classList.add('is-invalid');
+  }
 
-  const form = event.target;
+  function addValidClass(element) {
+    element.classList.add('is-valid');
+  }
 
-  const query = `name=${form.name.value}&gender=${form.gender.value}`;
+  form.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
 
-  const res = await fetch(`/api/update/user/${id}?${query}`);
-  const response = await res.json();
-  console.log(response);
-};
+    for (let input of inputs) {
+      const lenght = input.value.length;
+      const valid_length = lenght > 3;
+      if (!input.value || input.value === ' ') {
+        addInvalidClass(input);
+        nameInvalidFeedback.innerHTML = 'Please, enter your name.';
+        return;
+      } else if (!valid_length) {
+        addInvalidClass(input);
+        nameInvalidFeedback.innerHTML = 'Must be at least 4 characters long';
+        return;
+      } else if (!valid_pattern.test(input.value)) {
+        addInvalidClass(input);
+        nameInvalidFeedback.innerHTML = 'Some symbols are not allowed';
+        return;
+      } else {
+        input.classList.remove('is-invalid');
+        addValidClass(input);
+      }
+    }
+    const search = `?name=${nameInput.value}&gender=${genderInput.value}`;
+    await fetch(`${window.location.pathname}${search}`);
+    window.location.pathname = '/users.html';
+  });
+}
 
-document.forms[0].addEventListener('submit', handleFormSubmit);
+window.addEventListener('load', isFormValid);
